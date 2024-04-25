8e849be90749d34f64a05040b0488071a087319b
LeeHyoSeok <lhs19960330@gmail.com>
2024-04-25 09:37:48 +0900

    feat : 네트워크 채팅 만들기

---
+++ Assets/Lobby/Scripts/LobbyManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    public enum Panel { Login, Menu, Lobby, Room }
+
+    [SerializeField] LoginPanel loginPanel;
+    [SerializeField] MainPanel menuPanel;
+    [SerializeField] RoomPanel roomPanel;
+    [SerializeField] LobbyPanel lobbyPanel;
+
+    private void Start()
+    {
+        SetActivePanel(Panel.Login);
+    }
+
+    private void SetActivePanel(Panel panel)
+    {
+        loginPanel.gameObject.SetActive(panel == Panel.Login);
+        menuPanel.gameObject.SetActive(panel == Panel.Menu);
+        roomPanel.gameObject.SetActive(panel == Panel.Room);
+        lobbyPanel.gameObject.SetActive(panel == Panel.Lobby);
+    }
+}
---
+++ Assets/Lobby/Scripts/MainPanel.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+
+public class MainPanel : MonoBehaviour
+{
+    [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject createRoomPanel;
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_InputField maxPlayerInputField;
+
+    public void CreateRoomMenu()
+    {
+
+    }
+
+    public void CreateRoomConfirm()
+    {
+
+    }
+
+    public void CreateRoomCancel()
+    {
+
+    }
+
+    public void RandomMatching()
+    {
+
+    }
+
+    public void JoinLobby()
+    {
+
+    }
+
+    public void Logout()
+    {
+
+    }
+}
